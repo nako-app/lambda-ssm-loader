718d5e3f7786f1b8edd2b7c44ff2e78682b5c6d8
Vincent Seguin <vincent.seguin.2@gmail.com>
2021-01-21 11:20:18 -0500

    Formatting and specify prefix

--- config.js
+++ config.js
@@ -1,33 +1,31 @@
-const { snakeCase } = require('change-case')
-const { ssm } = require('./aws')
+const { snakeCase } = require("change-case");
+const { ssm } = require("./aws");
 
-const environment = process.env.ENVIRONMENT || 'dev'
-const prefix = `/${environment}/api`
-
-const initializeConfig = async () => {
-  let nextToken = null
-  let hasNext = true
+const initializeConfig = async (prefix) => {
+  let nextToken = null;
+  let hasNext = true;
 
   while (hasNext) {
     const params = {
       NextToken: nextToken,
       Path: prefix,
       Recursive: true,
-      WithDecryption: true
-    }
+      WithDecryption: true,
+    };
 
-    const parameters = await ssm.getParametersByPath(params).promise()
+    const parameters = await ssm.getParametersByPath(params).promise();
 
-    console.log(parameters)
-    parameters.Parameters.forEach(p => {
-      process.env[snakeCase(p.Name.replace(prefix, '')).toUpperCase()] = p.Value
-    })
+    console.log(parameters);
+    parameters.Parameters.forEach((p) => {
+      process.env[snakeCase(p.Name.replace(prefix, "")).toUpperCase()] =
+        p.Value;
+    });
 
-    hasNext = parameters.NextToken != null
-    nextToken = parameters.NextToken
+    hasNext = parameters.NextToken != null;
+    nextToken = parameters.NextToken;
   }
-}
+};
 
 module.exports = {
-  initializeConfig
-}
+  initializeConfig,
+};
